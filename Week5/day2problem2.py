016711030e6e36b6c41629e50d36ee0896bbd75b
Jr Hector Gonzalez <jhgonzalez.tx@gmail.com>
2024-10-19 14:02:00 -0500

    We are now diving into learning how to make Linked Lists in Python and we started by creating two Pokemon nodes and linking them to each other

---
+++ Week5/day2problem2.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+node_1 = Node("Jigglypuff", None)
+node_2 = Node("Wigglytuff", None)
+
+node_1.next = node_2
+
+print(node_1.value, "->", node_1.next.value)
+print(node_2.value, "->", node_2.next)
